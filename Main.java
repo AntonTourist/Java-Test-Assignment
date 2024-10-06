6d8d858388ec3d2f76c7e553c9ffc056d680402a
AntonTourist <anton.chupakhin81@gmail.com>
2024-10-06 17:03:17 +0500

    Added user input functionality for tasks 1 and 2

--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Compare two integers
-        int a = 5;
-        int b = 10;
-        System.out.println("a is greater than b: " + (a > b));
+        Scanner scanner = new Scanner(System.in);
 
-        // Perform basic arithmetic operations
+        // 1. Ввод двух целых чисел и выполнение операций
+        System.out.println("Введите первое целое число (a):");
+        int a = scanner.nextInt();  // Пользователь вводит число a
+        System.out.println("Введите второе целое число (b):");
+        int b = scanner.nextInt();  // Пользователь вводит число b
+
+        // Сравнение чисел
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        // Выполнение арифметических операций
         int sum = a + b;
         int difference = b - a;
         int product = a * b;
@@ -15,18 +29,29 @@ public class Main {
         System.out.println("Product: " + product);
         System.out.println("Quotient: " + quotient);
 
-        // Compare two strings
-        String str1 = "Hello";
-        String str2 = "World";
-        System.out.println("Strings are equal: " + str1.equals(str2));
+        // 2. Ввод двух строк и их сравнение
+        System.out.println("Введите первую строку:");
+        scanner.nextLine();  // Считываем пустую строку после ввода чисел
+        String str1 = scanner.nextLine();  // Пользователь вводит первую строку
+        System.out.println("Введите вторую строку:");
+        String str2 = scanner.nextLine();  // Пользователь вводит вторую строку
 
-        // Output even numbers from an array
-        int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        System.out.println("Even numbers in the array:");
+        // Сравнение строк
+        if (str1.equals(str2)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
+        }
+
+        // 3. Вывод четных чисел из массива
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Четные числа в массиве:");
         for (int number : numbers) {
             if (number % 2 == 0) {
                 System.out.println(number);
             }
         }
+
+        scanner.close();
     }
 }
